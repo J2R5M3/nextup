2db385709e113c26ee9adfa22479dd71571927cc
Brian Kehrig <briankehrig@gmail.com>
2021-04-12 21:47:34 -0600

    Added more apps

--- nextup_api/apps.py
+++ nextup_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class NextupApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'nextup_api'
---
+++ nextup_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NextupSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nextup_site'
