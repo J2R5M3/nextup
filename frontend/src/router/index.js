ec40e8eaf656c6978f582479a0cc560402f9bf3e
ArcticGlacier <81803255+ArcticGlacier@users.noreply.github.com>
2021-04-15 00:27:40 -0600

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,14 @@ import Router from 'vue-router'
 import createaccount from '@/components/createaccount'
 import login from '@/components/login'
 import room from '@/components/room'
+import admin from '@/components/admin'
+import joinroom from '@/components/joinroom'
+import hostroom from '@/components/hostroom'
+import inroom from '@/components/inroom'
+import suggestions from '@/components/suggestions'
+import add from '@/components/add'
+import deletemedia from '@/components/deletemedia'
+import review from '@/components/review'
 
 Vue.use(Router)
 
@@ -22,6 +30,46 @@ export default new Router({
       path: '/room',
       name: 'room',
       component: room
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: admin
+    },
+    {
+      path: '/joinroom',
+      name: 'joinroom',
+      component: joinroom
+    },
+    {
+      path: '/hostroom',
+      name: 'hostroom',
+      component: hostroom
+    },
+    {
+      path: '/inroom',
+      name: 'inroom',
+      component: inroom
+    },
+    {
+      path: '/suggestions',
+      name: 'suggestions',
+      component: suggestions
+    },
+    {
+      path: '/add',
+      name: 'add',
+      component: add
+    },
+    {
+      path: '/deletemedia',
+      name: 'deletemedia',
+      component: deletemedia
+    },
+    {
+      path: '/review',
+      name: 'review',
+      component: review
     }
   ]
 })
